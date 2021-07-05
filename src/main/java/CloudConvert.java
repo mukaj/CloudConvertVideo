a23a211318cb5adeaf5047b596720d34d73a4491
mukaj <43552866+mukaj@users.noreply.github.com>
2021-07-05 03:16:42 -0400

    Implemented file naming
    Implemented setting converted filename as a conversion task option. Filename must not contain extension however.
    
    Cleaned up code by removing unnecessary variables and unnecessary declarations.

--- src/main/java/CloudConvert.java
+++ src/main/java/CloudConvert.java
@@ -6,6 +6,7 @@ import com.cloudconvert.dto.request.UrlExportRequest;
 import com.cloudconvert.dto.request.UrlImportRequest;
 import com.cloudconvert.dto.response.JobResponse;
 import com.cloudconvert.dto.response.TaskResponse;
+import com.cloudconvert.dto.result.Result;
 import com.google.common.collect.ImmutableMap;
 import org.apache.tika.io.IOUtils;
 
@@ -21,6 +22,12 @@ public class CloudConvert {
 
     public CloudConvert() throws IOException {}
 
+    /**
+     *
+     * @param task Conversion task
+     * @param properties HashMap for all the conversion settings
+     * @return
+     */
     private ConvertFilesTaskRequest setProperties(ConvertFilesTaskRequest task, HashMap<String, String> properties) {
         properties.forEach((key, value) ->
                 task.setProperty(key, value));
@@ -35,29 +42,28 @@ public class CloudConvert {
      * @param convertedOutputFile Converted File
      * @param inputType Extension of input file
      * @param outputType Extension of output file
+     * @param outputName Name of output file (MUST NOT INCLUDE EXTENSION)
      * @param inOptions Custom Options for Conversion
      * @throws IOException
      * @throws URISyntaxException
      * @throws InterruptedException
      */
-    public void convert(final String inputFileURL, File convertedOutputFile, String inputType, String outputType,
+    public void convert(final String inputFileURL, File convertedOutputFile, String outputType,
                         final String outputName, HashMap<String, String> inOptions) throws IOException, URISyntaxException, InterruptedException {
 
-        //Creating ImmutableMap for the Job
-        ImmutableMap jobMap = ImmutableMap.of(
+        // Creating the convert task request and applying the options to it.
+        final ConvertFilesTaskRequest conversionTask = setProperties(new ConvertFilesTaskRequest(), inOptions);
+
+        // Create a job to import, convert and export the video
+        final JobResponse createJobResponse = (JobResponse) cloudConvertClient.jobs().create(
+                ImmutableMap.of(
                 "ImportVideo", new UrlImportRequest().setUrl(inputFileURL),
-                "ConvertVideo", new ConvertFilesTaskRequest()
+                "ConvertVideo", conversionTask
+                        .setFilename(outputName.concat(".").concat(outputType))
                         .setInput("ImportVideo")
-                        .setInputFormat(inputType)
                         .setOutputFormat(outputType),
                 "ExportVideo", new UrlExportRequest().setInput("ConvertVideo")
-        );
-        // Setting Properties
-        ConvertFilesTaskRequest conversionTask = (ConvertFilesTaskRequest) jobMap.get("ConvertVideo");
-        conversionTask = setProperties((ConvertFilesTaskRequest) conversionTask, inOptions);
-
-        // Create a job to Convert and Export Video
-        final JobResponse createJobResponse = (JobResponse) cloudConvertClient.jobs().create(jobMap).getBody();
+        )).getBody();
 
         //Get Export TaskResponse
         final TaskResponse waitUrlExportTaskResponse = cloudConvertClient.tasks().wait(createJobResponse.getTasks().get(2).getId()).getBody();
@@ -68,16 +74,9 @@ public class CloudConvert {
         InputStream convertedFile = cloudConvertClient.files().download(exportUrl).getBody();
         OutputStream outputStream = null;
 
-        // Naming the converted file
-        if(outputName.isEmpty() || outputName == null) {
-            /* If there was no name given, or a blank name given at the func. arguments
-            it will get the origial name of the file */
-            final String filename = waitUrlExportTaskResponse.getResult().getFiles().get(0).get("filename");
-            outputStream = new FileOutputStream(new File(filename));
-        }
-        else {
-            outputStream = new FileOutputStream(new File(outputName.concat(".").concat(outputType)));
-        }
+        // Creating an output stream to Save the converted file
+        final String filename = waitUrlExportTaskResponse.getResult().getFiles().get(0).get("filename");
+        outputStream = new FileOutputStream(new File(filename));
 
         IOUtils.copy(convertedFile, outputStream);
 
@@ -92,37 +91,31 @@ public class CloudConvert {
      *
      * @param inputFile File we want to Convert
      * @param convertedOutputFile Converted File
-     * @param inputType Extension of input file
      * @param outputType Extension of output file
+     * @param outputName Name of output file (MUST NOT INCLUDE EXTENSION)
      * @param inOptions Custom Options for Conversion
      * @throws IOException
      * @throws URISyntaxException
      * @throws InterruptedException
      */
-    public void convert(final File inputFile, File convertedOutputFile, String inputType, String outputType,
+    public void convert(final File inputFile, File convertedOutputFile, String outputType,
                         final String outputName, HashMap<String, String> inOptions) throws IOException, URISyntaxException, InterruptedException {
 
         // Upload file using import/upload task
         final TaskResponse uploadImportTaskResponse = cloudConvertClient.importUsing().upload(new UploadImportRequest(), inputFile).getBody();
 
-        // Wait for import/upload task to be finished
-        final TaskResponse waitUploadImportTaskResponse = cloudConvertClient.tasks().wait(uploadImportTaskResponse.getId()).getBody();
-
-        // Creating ImmutableMap for the Job
-        ImmutableMap jobMap = ImmutableMap.of(
-                "ConvertVideo", new ConvertFilesTaskRequest()
-                        .setInput(waitUploadImportTaskResponse.getId())
-                        .setInputFormat(inputType)
-                        .setOutputFormat(outputType),
-                "ExportVideo", new UrlExportRequest().setInput("ConvertVideo")
-        );
-
-        // Setting Properties
-        ConvertFilesTaskRequest conversionTask = (ConvertFilesTaskRequest) jobMap.get("ConvertVideo");
-        conversionTask = setProperties((ConvertFilesTaskRequest) conversionTask, inOptions);
+        // Creating the convert task request and applying the options to it.
+        final ConvertFilesTaskRequest conversionTask = setProperties(new ConvertFilesTaskRequest(), inOptions);
 
         // Create a job to Convert and Export Video
-        final JobResponse createJobResponse = (JobResponse) cloudConvertClient.jobs().create(jobMap).getBody();
+        final JobResponse createJobResponse = cloudConvertClient.jobs().create(
+            ImmutableMap.of(
+                    "ConvertVideo", conversionTask
+                            .setFilename(outputName.concat(".").concat(outputType))
+                            .setInput(uploadImportTaskResponse.getId())
+                            .setOutputFormat(outputType),
+                    "ExportVideo", new UrlExportRequest().setInput("ConvertVideo")
+            )).getBody();
 
         //Get Export TaskResponse
         final TaskResponse waitUrlExportTaskResponse = cloudConvertClient.tasks().wait(createJobResponse.getTasks().get(1).getId()).getBody();
@@ -133,16 +126,9 @@ public class CloudConvert {
         InputStream convertedFile = cloudConvertClient.files().download(exportUrl).getBody();
         OutputStream outputStream = null;
 
-        // Naming the converted file
-        if(outputName.isEmpty() || outputName == null) {
-            /* If there was no name given, or a blank name given at the func. arguments
-            it will get the origial name of the file */
-            final String filename = waitUrlExportTaskResponse.getResult().getFiles().get(0).get("filename");
-            outputStream = new FileOutputStream(new File(filename));
-        }
-        else {
-            outputStream = new FileOutputStream(new File(outputName));
-        }
+        // Creating an output stream to Save the converted file
+        final String filename = waitUrlExportTaskResponse.getResult().getFiles().get(0).get("filename");
+        outputStream = new FileOutputStream(new File(filename));
 
 
         IOUtils.copy(convertedFile, outputStream);
@@ -165,11 +151,11 @@ public class CloudConvert {
         testmap.put("volume", "0");
 
         // Uncomment to Test url
-        //convertClient.convert("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",output,"mp4", "webm", "convertedFile",testmap);
+        convertClient.convert("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",output, "webm", "convertedFile",testmap);
 
         // Uncomment to Test LocalFile
-        //File input = new File("testVideo.mp4");
-        //convertClient.convert(input,output,"mp4", "webm", "converted.webm",testmap);
+        File input = new File("testVideo.mp4");
+        convertClient.convert(input,output, "webm", "converted",testmap);
         return;
     }
 }
